a26457fbb802fb87614ed207bdc99b4b944e61ef
Paritosh Yadav <paritosh.yadav.237@gmail.com>
2021-02-20 23:26:52 +0530

    swagger def update

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,19 +1,14 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-mongoose-boilerplate API documentation',
+    title: 'Device Inventory API documentation',
     version,
-    license: {
-      name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
-    },
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `https://device-inventory.herokuapp.com/v1`,
     },
   ],
 };
