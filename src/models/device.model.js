ae51c0faa608d921dd531c2d2b6c2fa585491adc
Paritosh Yadav <paritosh.yadav.237@gmail.com>
2021-03-19 21:47:01 +0530

    Validation & test case added for device model

--- src/models/device.model.js
+++ src/models/device.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const { toJSON, paginate } = require('./plugins');
 
 const deviceSchema = new mongoose.Schema(
@@ -7,33 +8,64 @@ const deviceSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      validate(value) {
+        // Blacklisting the '' (space), as its not part of isAlpha
+        if (!validator.isAlpha(validator.blacklist(value, ' '))) {
+          throw new Error('Modal name should conatin only alphabets');
+        }
+      },
     },
     srNo: {
       type: String,
       required: true,
       trim: true,
       unique: true,
+      validate(value) {
+        if (!validator.isAlphanumeric(value)) {
+          throw new Error('Serial Number should be alphanumeric');
+        }
+      },
     },
     uuid: {
       type: String,
       required: true,
       trim: true,
       unique: true,
+      validate(value) {
+        if (!validator.isAlphanumeric(value)) {
+          throw new Error('UUID should be alphanumeric');
+        }
+      },
     },
     variant: {
       type: String,
       required: true,
       trim: true,
+      validate(value) {
+        if (!validator.isAlphanumeric(value)) {
+          throw new Error('Variant should be alphanumeric');
+        }
+      },
     },
     category: {
       type: String,
       required: true,
       trim: true,
+      validate(value) {
+        if (!validator.isAlpha(value)) {
+          throw new Error('Category should conatin only alphabets');
+        }
+      },
     },
     manufacturer: {
       type: String,
       required: true,
       trim: true,
+      validate(value) {
+        if (!validator.isAlpha(value)) {
+          throw new Error('Manufacturer name should conatin only alphabets');
+        }
+      },
     },
     isIssued: {
       type: Boolean,
--- tests/unit/models/device.model.test.js
+++ tests/unit/models/device.model.test.js
@@ -6,19 +6,31 @@ describe('Device model', () => {
     let newDevice;
     beforeEach(() => {
       newDevice = {
-        modalName: faker.name.findName(),
+        modalName: faker.lorem.word(),
         srNo: faker.random.alphaNumeric(),
         uuid: faker.random.alphaNumeric(),
         variant: faker.random.alphaNumeric(),
-        category: faker.random.word(),
-        manufacturer: faker.random.word(),
+        category: faker.lorem.word(),
+        manufacturer: faker.lorem.word(),
       };
     });
     test('should correctly validate a valid device', async () => {
       await expect(new Device(newDevice).validate()).resolves.toBeUndefined();
     });
-    test('should throw validation error is modalName in not string', async () => {
+    test('should throw validation error is modalName does not contain alphabets', async () => {
       newDevice.modalName = 123;
+      await expect(new Device(newDevice).validate()).rejects.toThrow();
+    });
+    test('should throw validation error is serial number is not alphanumeric', async () => {
+      newDevice.srNo = '#@$%^%!';
+      await expect(new Device(newDevice).validate()).rejects.toThrow();
+    });
+    test('should throw validation error is UUID is not alphanumeric', async () => {
+      newDevice.uuid = '#@$%^%!';
+      await expect(new Device(newDevice).validate()).rejects.toThrow();
+    });
+    test('should throw validation error is varient is not alphanumeric', async () => {
+      newDevice.variant = '#@$%^%!';
       await expect(new Device(newDevice).validate()).rejects.toThrow();
     });
   });
