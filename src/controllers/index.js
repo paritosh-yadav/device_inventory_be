740647fe9d5b02a8c6221085a8261fe9a41d1286
Paritosh Yadav <paritosh.yadav.237@gmail.com>
2022-04-14 11:20:23 +0530

    AASA file hosted as static file, reverted previous code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,3 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.deviceController = require('./device.controller');
 module.exports.deviceTransactionController = require('./deviceTransaction.controller');
-module.exports.aasaController = require('./aasa.controller');
